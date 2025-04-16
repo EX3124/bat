a0cd2ce851efe5ed097cc9804109be0c72955494
EX3124 <70867298+EX3124@users.noreply.github.com>
2025-04-16 08:31:41 +0800

    兼容文件名特殊字符

--- 刷recovery.bat
+++ 刷recovery.bat
@@ -2,10 +2,6 @@
 chcp 65001
 setlocal enabledelayedexpansion
 
-REM 全局变量
-set DOWNLOAD_URL=https://dl.google.com/android/repository/platform-tools-latest-windows.zip
-set EXTRACT_DIR=extract
-
 echo github.com/EX3124/bat
 echo.
 echo.
@@ -13,25 +9,24 @@ echo.
 echo.
 
 :DOWNLOAD
-if exist %EXTRACT_DIR% (
-    rmdir /S /Q %EXTRACT_DIR%
+if exist temp (
+    rmdir /S /Q temp
 )
 if exist platform-tools-latest-windows.zip (
     del platform-tools-latest-windows.zip
 )
 
-echo 下载Android SDK Platform-Tools
-powershell -Command "Invoke-WebRequest -Uri %DOWNLOAD_URL% -OutFile platform-tools-latest-windows.zip"
+echo 下载 Android SDK Platform-Tools
+powershell -Command "Invoke-WebRequest -Uri https://dl.google.com/android/repository/platform-tools-latest-windows.zip -OutFile platform-tools-latest-windows.zip"
 if %ERRORLEVEL% neq 0 (
     echo 下载失败,正在重试
     goto DOWNLOAD
 )
 
-REM 解压
-if not exist %EXTRACT_DIR% (
-    mkdir %EXTRACT_DIR%
+if not exist temp (
+    mkdir temp
 )
-tar -xf platform-tools-latest-windows.zip -C %EXTRACT_DIR%
+tar -xf platform-tools-latest-windows.zip -C temp
 del platform-tools-latest-windows.zip
 cls
 
@@ -42,24 +37,12 @@ set FILE_PATH=%FILE_PATH:"=%
 cls
 echo 已选择的文件: %FILE_PATH%
 
-REM 检查文件是否存在
 if not exist "%FILE_PATH%" (
     echo 找不到文件,请确认它是否可用
     echo.
     goto INPUT
 )
 
-REM 去除括号
-for %%f in ("%FILE_PATH%") do (
-    set FILE_NAME=%%~nxf
-    set NEW_FILE_NAME=%%~nxf
-)
-set NEW_FILE_NAME=%NEW_FILE_NAME:(=%
-set NEW_FILE_NAME=%NEW_FILE_NAME:) =%
-ren "%FILE_PATH%" "%NEW_FILE_NAME%"
-set FILE_PATH=%~dp0%NEW_FILE_NAME%
-
-REM 获取文件名
 for %%f in ("%FILE_PATH%") do (
     set FILE_NAME=%%~nxf
     set FILE_SIZE=%%~zf
@@ -70,8 +53,7 @@ for %%f in ("%FILE_PATH%") do (
     )
 )
 
-REM 检查文件名是否包含指定字样
-set "EXTENSION=%FILE_NAME:~-3%"
+set EXTENSION=%FILE_NAME:~-3%
 if "%EXTENSION%" == "zip" (
     if not "%FILE_NAME:rec=%"=="%FILE_NAME%" (
         goto EXTRACT_ZIP
@@ -84,20 +66,21 @@ if "%EXTENSION%" == "zip" (
         echo.
         goto INPUT
     )
-) else if "%EXTENSION%"=="img" (
-    copy %FILE_PATH% %EXTRACT_DIR% >nul 2>&1
-    goto CHECK_FASTBOOT
-) else (
-    echo 不是.zip或.img文件
-    echo.
-    goto INPUT
 )
+if "%EXTENSION%" == "img" (
+    copy %FILE_PATH% temp >nul 2>&1
+    set FILE_PATH=%FILE_PATH:"=%
+    goto CHECK_FASTBOOT
+)
+echo 不是.zip或.img文件
+echo.
+goto INPUT
 
 :EXTRACT_ZIP
-if not exist %EXTRACT_DIR%\%FILE_NAME:~0,-4% (
-    mkdir %EXTRACT_DIR%\%FILE_NAME:~0,-4%
+if not exist "temp\%FILE_NAME:~0,-4%" (
+    mkdir "temp\%FILE_NAME:~0,-4%"
 )
-tar -xf "%FILE_PATH%" -C %EXTRACT_DIR%\%FILE_NAME:~0,-4% >nul 2>&1
+tar -xf "%FILE_PATH%" -C "temp\%FILE_NAME:~0,-4%" >nul 2>&1
 if !ERRORLEVEL! neq 0 (
     echo 解压失败,请检查文件是否损坏
     echo.
@@ -107,29 +90,27 @@ if !ERRORLEVEL! neq 0 (
 :CHECK_FASTBOOT
 cls
 echo 已选择的文件: %FILE_PATH%
-set DEVICE_FOUND=false
 set DEVICE_COUNT=0
-for /f "tokens=1" %%i in ('%EXTRACT_DIR%\platform-tools\fastboot.exe devices') do (
+for /f "tokens=1" %%i in ('temp\platform-tools\fastboot.exe devices') do (
     set DEVICE=%%i
-    set DEVICE_FOUND=true
     set /a DEVICE_COUNT+=1
 )
-if "%DEVICE_FOUND%"=="false" (
+if %DEVICE_COUNT% equ 0 (
     goto NO_DEVICE_FOUND
 )
 if %DEVICE_COUNT% gtr 1 (
     echo.
-    echo 检测到多台设备,请断开它们,只留一台设备
+    echo 检测到多台设备,请断开它们,只连接一台设备
     echo.
-    goto ERETRY
+    goto RE
 ) 
 echo 已连接的设备: %DEVICE%
 echo.
 
 :FLASH_IMG
-for /R %EXTRACT_DIR% %%i in (*.img) do (
+for /R temp %%i in (*.img) do (
     echo 使用img文件: %%i
-    for /f "tokens=*" %%j in ('%EXTRACT_DIR%\platform-tools\fastboot.exe flash recovery %%i 2^>^&1') do (
+    for /f "tokens=*" %%j in ('temp\platform-tools\fastboot.exe flash recovery "%%i" 2^>^&1') do (
         set "CHECK_FLASH=%%j"
         echo !CHECK_FLASH!
     )
@@ -137,19 +118,19 @@ for /R %EXTRACT_DIR% %%i in (*.img) do (
         echo 刷入失败
         goto CHECK_FASTBOOT
     )
-    %EXTRACT_DIR%\platform-tools\fastboot.exe reboot recovery >nul 2>&1
-    goto CLEANUP
+    temp\platform-tools\fastboot.exe reboot recovery >nul 2>&1
+    goto DONE
 )
 
 :NO_DEVICE_FOUND
 echo.
 echo 没有检测到设备在fastboot模式下
 echo.
-echo 请检查设备是否进入了fastboot模式(页面带有 蓝色或橙色 FASTBOOT 字样)
-echo 如果已进入fastboot模式,请检查设备与电脑的连接,以及是否安装了小米线刷工具内的驱动
+echo 请检查手机是否进入了fastboot模式(页面带有蓝色或者橙色FASTBOOT字样)
+echo 如果已进入fastboot模式,请检查手机与电脑的连接,以及是否安装了小米线刷工具内的驱动
 echo.
 
-:ERETRY
+:RE
 set /p RETRY="重试? (y/n)"
 if /I "%RETRY%"=="y" (
     goto CHECK_FASTBOOT
@@ -158,13 +139,14 @@ if /I "%RETRY%"=="y" (
 ) else if /I "%RETRY%"=="是" (
     goto CHECK_FASTBOOT
 ) else (
-    rmdir /s /q %EXTRACT_DIR%\%FILE_NAME:~0,-4%
+    del "temp\%FILE_NAME%"
+    rmdir /S /Q "temp\%FILE_NAME:~0,-4%"
     cls
     goto INPUT
 )
 
-:CLEANUP
-rmdir /S /Q %EXTRACT_DIR%
+:DONE
+rmdir /S /Q temp
 echo.
 echo 运行结束
 
